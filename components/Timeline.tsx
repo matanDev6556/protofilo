32dc4b55753e088329c0936d0f26c767553683da
Matan kalifa <>
2025-03-20 10:24:09 +0200

    add cretifacte for studys

--- components/Timeline.tsx
+++ components/Timeline.tsx
@@ -9,18 +9,20 @@ const experiences = [
       "Pursuing a Bachelor's degree in Software Engineering, gaining expertise in algorithms, data structures, and software development principles.",
   },
   {
-    year: '2025 - Present',
+    year: '2025',
     title: 'Full-Stack Flutter & Node.js Course',
     company: 'Udemy',
     description:
       'Currently enrolled in a hands-on course covering Flutter, Node.js, Express.js, and MongoDB, focusing on building full-stack applications with modern technologies.',
+    certificate: '/Udemy full stack certificate.pdf',
   },
   {
     year: '2018',
     title: 'Android App Development Bootcamp',
     company: 'Certified Program',
     description:
-      'Completed an intensive 200+ hour bootcamp focused on Android application development, covering Java, Kotlin, and modern app architecture.',
+      'Completed an intensive 300+ hour bootcamp focused on Android application development, covering Java, Kotlin, and modern app architecture.',
+    certificate: '/android certifacte.pdf',
   },
 ];
 
@@ -40,6 +42,18 @@ export default function Timeline() {
           <h3 className="text-xl font-semibold mb-1">{exp.title}</h3>
           <div className="text-gray-400 mb-2">{exp.company}</div>
           <p className="text-gray-500">{exp.description}</p>
+
+          {exp.certificate && (
+            <div className="mt-5 text-center ">
+              <a
+                href={exp.certificate}
+                download
+                className="px-4 py-2 bg-[#FFD760] text-black rounded-lg hover:bg-yellow-500 transition"
+              >
+                Certificate
+              </a>
+            </div>
+          )}
         </motion.div>
       ))}
     </div>
